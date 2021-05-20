05d259738c4a5ff7e9aa89bc46ddb5fe6730bd44
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-05-20 10:59:03 -0500

    play, pause, prev, and next buttons work, music is playing, artist info is showing

---
+++ js/script.js
@@ -0,0 +1,66 @@
+const wrapper = document.querySelector(".wrapper"),
+musicImg = wrapper.querySelector(".img-area img"),
+musicName = wrapper.querySelector(".song-details .name"),
+musicArtist = wrapper.querySelector(".song-details .artist"),
+mainAudio = wrapper.querySelector("#main-audio"),
+playPauseBtn = wrapper.querySelector(".play-pause"),
+prevBtn = wrapper.querySelector("#prev"),
+nextBtn = wrapper.querySelector("#next"),
+progressBar = wrapper.querySelector(".progress-bar");
+
+let musicIndex = 4;
+
+window.addEventListener("load", () => {
+  loadMusic(musicIndex); // called once window loads
+})
+
+function loadMusic(indexNumb) {
+  musicName.innerText = allMusic[indexNumb - 1].name;
+  musicArtist.innerText = allMusic[indexNumb - 1].artist;
+  musicImg.src = `images/${allMusic[indexNumb - 1].img}.jpg`;
+  mainAudio.src = `songs/${allMusic[indexNumb - 1].src}.mp3`;
+}
+
+function playMusic() {
+  wrapper.classList.add("paused");
+  playPauseBtn.querySelector("i").innerText = "pause";
+  mainAudio.play();
+}
+
+function pauseMusic() {
+  wrapper.classList.remove("paused");
+  playPauseBtn.querySelector("i").innerText = "play_arrow";
+  mainAudio.pause();
+}
+
+function nextMusic() {
+  musicIndex++;
+  // if musicIndex is greater than array length then MusicIndex will go to first song
+  musicIndex > allMusic.length ? musicIndex = 1 : musicIndex = musicIndex;
+  loadMusic(musicIndex);
+  playMusic();
+}
+
+function prevMusic() {
+  musicIndex--;
+  musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex;
+  loadMusic(musicIndex);
+  playMusic();
+}
+
+playPauseBtn.addEventListener("click", () => {
+  const isMusicPaused = wrapper.classList.contains("paused");
+  isMusicPaused ? pauseMusic() : playMusic();
+});
+
+nextBtn.addEventListener("click", () => {
+  nextMusic();
+});
+
+prevBtn.addEventListener("click", () => {
+  prevMusic();
+});
+
+mainAudio.addEventListener("timeupdate", (e) => {
+  
+})
