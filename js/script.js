778120837b8c61a7b2eabb03a2acff836d88856c
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-05-20 12:23:07 -0500

    showing song details when click on more music

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 const wrapper = document.querySelector(".wrapper"),
-musicImg = wrapper.querySelector(".img-area img"),
-musicName = wrapper.querySelector(".song-details .name"),
-musicArtist = wrapper.querySelector(".song-details .artist"),
-mainAudio = wrapper.querySelector("#main-audio"),
-playPauseBtn = wrapper.querySelector(".play-pause"),
-prevBtn = wrapper.querySelector("#prev"),
-nextBtn = wrapper.querySelector("#next"),
-progressBar = wrapper.querySelector(".progress-bar"),
-progressArea = wrapper.querySelector(".progress-area");
+  musicImg = wrapper.querySelector(".img-area img"),
+  musicName = wrapper.querySelector(".song-details .name"),
+  musicArtist = wrapper.querySelector(".song-details .artist"),
+  mainAudio = wrapper.querySelector("#main-audio"),
+  playPauseBtn = wrapper.querySelector(".play-pause"),
+  prevBtn = wrapper.querySelector("#prev"),
+  nextBtn = wrapper.querySelector("#next"),
+  progressArea = wrapper.querySelector(".progress-area"),
+  progressBar = wrapper.querySelector(".progress-bar"),
+  musicList = wrapper.querySelector(".music-list"),
+  showMoreBtn = wrapper.querySelector("#more-music"),
+  hideMusicBtn = musicList.querySelector("#close");
 
 let musicIndex = 4;
 
 window.addEventListener("load", () => {
   loadMusic(musicIndex); // called once window loads
-})
+});
 
 function loadMusic(indexNumb) {
   musicName.innerText = allMusic[indexNumb - 1].name;
@@ -37,14 +40,14 @@ function pauseMusic() {
 function nextMusic() {
   musicIndex++;
   // if musicIndex is greater than array length then MusicIndex will go to first song
-  musicIndex > allMusic.length ? musicIndex = 1 : musicIndex = musicIndex;
+  musicIndex > allMusic.length ? (musicIndex = 1) : (musicIndex = musicIndex);
   loadMusic(musicIndex);
   playMusic();
 }
 
 function prevMusic() {
   musicIndex--;
-  musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex;
+  musicIndex < 1 ? (musicIndex = allMusic.length) : (musicIndex = musicIndex);
   loadMusic(musicIndex);
   playMusic();
 }
@@ -69,10 +72,9 @@ mainAudio.addEventListener("timeupdate", (e) => {
   progressBar.style.width = `${progressWidth}%`;
 
   let musicCurrentTime = wrapper.querySelector(".current"),
-  musicDuration = wrapper.querySelector(".duration");
+    musicDuration = wrapper.querySelector(".duration");
 
   mainAudio.addEventListener("loadeddata", () => {
-
     // show song total duration
     let audioDuration = mainAudio.duration;
     let totalMin = Math.floor(audioDuration / 60);
@@ -99,4 +101,86 @@ progressArea.addEventListener("click", (e) => {
   let songDuration = mainAudio.duration;
 
   mainAudio.currentTime = (clickedOffSetX / progressWidthVal) * songDuration;
-})
+});
+
+const repeatBtn = wrapper.querySelector("#repeat-plist");
+repeatBtn.addEventListener("click", () => {
+  // get the inner text of the icon and then update
+  let getText = repeatBtn.innerText;
+  switch (getText) {
+    case "repeat": // if this icon is repeat
+      repeatBtn.innerText = "repeat_one";
+      repeatBtn.setAttribute("title", "Song looped");
+      break;
+    case "repeat_one":
+      repeatBtn.innerText = "shuffle";
+      repeatBtn.setAttribute("title", "Playback shuffle");
+      break;
+    case "shuffle":
+      repeatBtn.innerText = "repeat";
+      repeatBtn.setAttribute("title", "Playlist looped");
+      break;
+  }
+});
+
+// what happens after a song ends according to the repeat/shuffle icon
+mainAudio.addEventListener("ended", () => {
+  let getText = repeatBtn.innerText;
+  switch (getText) {
+    case "repeat":
+      nextMusic();
+      break;
+    case "repeat_one":
+      mainAudio.currentTime = 0;
+      loadMusic(musicIndex);
+      playMusic();
+      break;
+    case "shuffle":
+      let randIndex = Math.floor(Math.random() * allMusic.length + 1);
+      do {
+        randIndex = Math.floor(Math.random() * allMusic.length + 1);
+      } while (musicIndex == randIndex);
+      musicIndex = randIndex;
+      loadMusic(musicIndex);
+      playMusic();
+      break;
+  }
+});
+
+showMoreBtn.addEventListener("click", () => {
+  musicList.classList.toggle("show");
+});
+
+hideMusicBtn.addEventListener("click", () => {
+  showMoreBtn.click();
+});
+
+const ulTag = wrapper.querySelector("ul");
+
+// create li according to the array length
+for (let i = 0; i < allMusic.length; i++) {
+  let liTag = `        
+    <li>
+      <div class="row">
+        <span>${allMusic[i].name}</span>
+        <p>${allMusic[i].artist}</p>
+      </div>
+      <audio class="${allMusic[i].src}" src="songs/${allMusic[i].src}.mp3"></audio>
+      <span id="${allMusic[i].src}" class="audio-duration">3:40</span>
+    </li>
+  `;
+  ulTag.insertAdjacentHTML("beforeend", liTag);
+
+  let liAudioDuration = ulTag.querySelector(`#${allMusic[i].src}`);
+  let liAudioTag = ulTag.querySelector(`.${allMusic[i].src}`);
+
+  liAudioTag.addEventListener("loadeddata", () => {
+    let audioDuration = liAudioTag.duration;
+    let totalMin = Math.floor(audioDuration / 60);
+    let totalSec = Math.floor(audioDuration % 60);
+    if (totalSec < 10) {
+      totalSec = `0${totalSec}`;
+    }
+    liAudioDuration.innerText = `${totalMin}:${totalSec}`;
+  })
+}
